f208777ead64b9ccdbb7d8c06d8ea88451fcef74
SANTOSH KOLI <santosh5796koli@gmail.com>
2026-04-26 20:55:13 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@ function buyNow(e){
   sessionStorage.setItem("reelsAccess","yes");
   window.location.href = e.currentTarget.href;
 }</script>
+<script>
+document.querySelectorAll(".pay-btn").forEach(function(btn) {
+  btn.addEventListener("click", function(e) {
+    e.preventDefault(); // redirect rokega
+
+    alert("🚧 Website Under Maintenance!\n\nPlease try again later.");
+  });
+});
+</script>
