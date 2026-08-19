bc0009ee92d7a92e4f7da9f6c488a57555050579
TranscriptAi <kiritokun5796@gmail.com>
2026-08-19 14:33:36 +0530

    Fix duplicate escapeHtml declaration syntax error v2.0.8

--- assets/js/download.js
+++ assets/js/download.js
@@ -1,12 +1,3 @@
-if (typeof window.escapeHtml !== "function") {
-    window.escapeHtml = function escapeHtml(str) {
-        return String(str || "").replace(/[&<>"']/g, match => ({
-            '&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&#39;'
-        }[match]));
-    };
-}
-const escapeHtml = window.escapeHtml;
-
 import {
     auth
 } from "./firebase-client.js";
