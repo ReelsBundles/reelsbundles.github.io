7f660e3387c3f5e4d747f80a70b3e65238e5a9ba
TranscriptAi <kiritokun5796@gmail.com>
2026-08-31 11:36:33 +0530

    fix(payment): define escapeHtml in payment.js to fix ReferenceError blocking available offers rendering

--- assets/js/payment.js
+++ assets/js/payment.js
@@ -25,6 +25,16 @@ const API_BASE =
             : "https://reelsbundles-backend.onrender.com"
     );
 
+function escapeHtml(str) {
+    if (!str) return '';
+    return String(str)
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#039;');
+}
+
 
 /*
  * Cashfree mode.
